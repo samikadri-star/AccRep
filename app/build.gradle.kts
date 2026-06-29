d6797d4d7d562abb776d6cc254311c754e598822
samikadri-star <mr.e2evil@gmail.com>
2026-06-29 21:03:46 +0300

    build: downgrade targetSdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
   defaultConfig {
     applicationId = "com.aistudio.expensecalculator.sami"
     minSdk = 24
-    targetSdk = 36
+    targetSdk = 35
     versionCode = 1
     versionName = "1.0"
 
